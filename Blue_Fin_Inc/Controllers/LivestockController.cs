b0f3c77e8bc03fdea4e86ba4aa7042de69babe12
Mike-Reina <michael.j.reina@hotmail.com>
2021-04-01 19:47:17 +0100

    Added code for viewing all Products to an Index view. Layout buttons at the top will allow you to access both Livestock and Equipment.

--- Blue_Fin_Inc/Controllers/LivestockController.cs
+++ Blue_Fin_Inc/Controllers/LivestockController.cs
@@ -10,18 +10,30 @@ namespace Blue_Fin_Inc.Controllers
 {
     public class LivestockController : Controller
     {
+        static List<Livestock> LivestockList = new List<Livestock>()
+        {
+            new Livestock(CareLevel.Easy, Temperment.Peaceful, WaterType.Fresh, "Black, Silver, Red", "PH:6.0-6.5, KH 0-10, 22°C-26°C", "5cm", 2001, "Harlequin Rasbora", "The Harlequin Rasbora is easily identified by its characteristic black pork chop shaped patch and beautifully lustrous copper/orange body", 2.99),
+            new Livestock(CareLevel.Easy, Temperment.Aggresive, WaterType.Fresh, "Black, Blue, Red", "PH:6.0-6.5, KH 0-10, 22°C-26°C", "7.5cm", 2002, "Crown Tail Betta", "The Crown Tail Betta has a striking, elaborate tail that differentiates it from other Bettas. The Crown Tail has a teardrop shape to its tail while the Twin Tail is split, almost giving the suggestion of having two tails.", 19.99)
+        };
+
         // GET: LivestockController
         public ActionResult Index()
         {
-            return View();
+            return View(LivestockList);
         }
 
         // GET: LivestockController/Details/5
-        public ActionResult Details()
+        public ActionResult Details(int id)
         {
-            Livestock livestock1 = new Livestock(CareLevel.Easy, Temperment.Peaceful, WaterType.Fresh, "Black, Silver, Red", "PH:6.0-6.5, KH 0-10, 22°C-26°C", "5cm", 2001, "Harlequin Rasbora", "The Harlequin Rasbora is easily identified by its characteristic black pork chop shaped patch and beautifully lustrous copper/orange body", 2.99);
-            livestock1.AddStock(10);
-            return View(livestock1);
+            Livestock foundLivestock = LivestockList.FirstOrDefault(p => p.ProductCode == id);
+            if (foundLivestock != null)
+            {
+                return View(foundLivestock);
+            }
+            else
+            {
+                return NotFound("No livestock found with product code: " + id + "\nIf this is new livestock please add it to the system.");
+            }
         }
 
         // GET: LivestockController/Create
@@ -33,15 +45,16 @@ namespace Blue_Fin_Inc.Controllers
         // POST: LivestockController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(Livestock newLivestock)
         {
-            try
+            if (ModelState.IsValid)
             {
-                return RedirectToAction(nameof(Index));
+                LivestockList.Add(newLivestock);
+                return View("Details");
             }
-            catch
+            else
             {
-                return View();
+                return View("Index");
             }
         }
 
