f53deb68ba08c69b0db78188b5b76ae16bab7f7c
Mike-Reina <michael.j.reina@hotmail.com>
2021-04-02 15:17:14 +0100

    Added CartEquipment and CartLivestock for use with Orders

---
+++ Blue_Fin_Inc/Models/CartLivestock.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blue_Fin_Inc.Models
+{
+    public class CartLivestock : Livestock
+    {
+        // Constructor
+        public CartLivestock(CareLevel _careLevel, Temperment _temperment, WaterType _waterType, string _colours, string _waterConditions, string _maxSize, int _productCode, string _name, string _discription, double _price) : 
+            base(_careLevel, _temperment, _waterType, _colours, _waterConditions, _maxSize, _productCode, _name, _discription, _price)
+        {
+            
+        }
+
+        // Methods 
+        public override void AddStock(int amount)
+        {
+            base.AddStock(amount);
+        }
+
+        public override void RemoveStock(int amount)
+        {
+            base.RemoveStock(amount);
+        }
+    }
+}
