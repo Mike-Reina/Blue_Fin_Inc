065711dc6c7c00c793c9a6cad51a5fe72c33895e
Mike-Reina <michael.j.reina@hotmail.com>
2021-04-04 16:55:19 +0100

    Changed layout for new order page with logo add to home page and sharded layout

--- Blue_Fin_Inc/Models/Product.cs
+++ Blue_Fin_Inc/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -62,6 +63,7 @@ namespace Blue_Fin_Inc.Models
         }
 
         [Required]
+        [DisplayName("Price(€)")]
         public double Price
         { 
             get => price;
