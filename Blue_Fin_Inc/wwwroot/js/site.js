907aba5bd540625a9f5d9bad6e44a830c4425eee
Eirini Galanaki <pirine91@gmail.com>
2021-04-05 12:51:29 +0100

    Facebook icon, address and javascript year functionallity added to the shared layout footer

--- Blue_Fin_Inc/wwwroot/js/site.js
+++ Blue_Fin_Inc/wwwroot/js/site.js
@@ -2,3 +2,6 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+//The year on the footer will change automatically each year
+var date = new Date;
+document.getElementById("year").innerHTML = date.getFullYear();
